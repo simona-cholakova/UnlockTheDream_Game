e5359606a70bf7c0278754f0301babaa29098143
simona-cholakova <colakovas@yahoo.com>
2025-12-02 14:11:14 +0100

    Better Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,34 +9,50 @@ public class PlayerMovement : MonoBehaviour
     float verticalInput;
     Vector3 moveDirection;
     Rigidbody rb;
+    public float acceleration = 10f;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>(); //gets the Rigidbody that is attached to the player
         rb.freezeRotation = true; //stops the player from tipping over when hitting walls
     }
+
+    //use Update() for input and use FixedUpdate() for physics
 
     void Update()
     {
         MyInput();
     }
 
-    void FixedUpdate()
+    void FixedUpdate() //unity runs this at a fixed rate
     {
         MovePlayer();
     }
 
     private void MyInput()
     {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        verticalInput = Input.GetAxisRaw("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
     }
 
     private void MovePlayer()
     {
         //calculate movement direction
-        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        //this is how the player always moves relative to where they look
         //orientation.forward points where you look, orientation.right points to your right side
-        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        Vector3 targetVelocity = moveDirection.normalized * moveSpeed;
+
+        Vector3 velocity = rb.linearVelocity;
+        Vector3 horizontalVel = new Vector3(velocity.x, 0f, velocity.z);
+
+        Vector3 velocityChange = targetVelocity - horizontalVel;
+
+        // limit max change per frame
+        velocityChange = Vector3.ClampMagnitude(velocityChange, acceleration);
+
+        rb.AddForce(velocityChange, ForceMode.VelocityChange);
+
+
     }
 }

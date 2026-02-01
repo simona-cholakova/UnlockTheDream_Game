ef2248ef07474a4eeabebf282d627750d36c9c4d
simona-cholakova <colakovas@yahoo.com>
2026-02-01 17:03:51 +0100

    Collecting keys audio added and lightning fixed

--- Assets/Scripts/KeyPickup.cs
+++ Assets/Scripts/KeyPickup.cs
@@ -2,11 +2,20 @@ using UnityEngine;
 
 public class KeyPickup : MonoBehaviour
 {
+
+    public AudioSource audio;
+
+    void Awake()
+    {
+        audio = GetComponent<AudioSource>();         
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             KeyManager.instance.AddKey();
+            audio.Play();
             Debug.Log("Key collected!");
         }
     }

36af8f41b3bd288de2c07823f4bbaa9b882f52da
simona-cholakova <colakovas@yahoo.com>
2026-02-13 13:32:09 +0100

    A little better movement of the big enemy but still with bugs

--- Assets/Scripts/bigEnemyThrow.cs
+++ Assets/Scripts/bigEnemyThrow.cs
@@ -15,11 +15,87 @@ public class bigEnemyThrow : MonoBehaviour
     public GameObject BlueBall;
     public float throwForce = 12f;
 
+    [Header("Ground Check Settings")]
+    public float rayHeight = 2f;
+    public float rayDistance = 5f;
+    public float yOffset = 0f; //set to half collider height MAYBE???
+
+    private float verticalVelocity = 0f;
+    private float gravity = -9.81f;
+
+
+    [Header("Movement")]
+    public float moveSpeed = 3f;
+    public float stopDistance = 17f;
+    CharacterController controller;
+
+
 
     private void OnEnable()
     {
         animator = GetComponent<Animator>();
     }
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        Renderer rend = GetComponentInChildren<Renderer>();
+        if (rend != null)
+        {
+            Vector3 size = rend.bounds.size;
+            float width = size.x;
+            float height = size.y;
+
+            controller.height = height;
+            controller.radius = width / 2f;
+            controller.center = new Vector3(0, height / 2f, 0);
+        }
+    }
+
+
+    // void MoveTowardsPlayer()
+    // {
+    //     Vector3 direction = playerObj.transform.position - transform.position;
+    //     direction.y = 0;              // ignore height
+    //     direction.Normalize();
+
+    //     transform.position += direction * moveSpeed * Time.deltaTime;
+    // }
+
+    // void MoveTowardsPlayer()
+    // {
+    //     Vector3 direction = playerObj.transform.position - transform.position;
+    //     direction.y = 0;
+    //     direction.Normalize();
+
+    //     Vector3 move = direction * moveSpeed;
+    //     move.y += Physics.gravity.y;
+
+    //     controller.Move(move * Time.deltaTime);
+    // }
+
+    void MoveTowardsPlayer()
+    {
+        Vector3 direction = playerObj.transform.position - transform.position;
+        direction.y = 0;
+        direction.Normalize();
+
+        //horizontal movement
+        Vector3 move = direction * moveSpeed;
+
+        //vertical movement
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -0.1f; //small value to keep grounded
+        }
+        else
+        {
+            verticalVelocity += gravity * Time.deltaTime;
+        }
+        move.y = verticalVelocity;
+
+        controller.Move(move * Time.deltaTime);
+    }
+
 
     private void Update()
     {
@@ -31,14 +107,24 @@ public class bigEnemyThrow : MonoBehaviour
 
         FacePlayer();
 
-        if (distanceBetweenObjects <= 40f)
+        // if (distanceBetweenObjects <= 40f)
+        // {
+        //     animator.SetBool("playerIsClose", true);
+        // }
+        // else
+        // {
+        //     animator.SetBool("playerIsClose", false);
+        // }
+        if (distanceBetweenObjects > stopDistance)
         {
-            animator.SetBool("playerIsClose", true);
+            animator.SetBool("playerIsClose", false);
+            MoveTowardsPlayer();
         }
         else
         {
-            animator.SetBool("playerIsClose", false);
+            animator.SetBool("playerIsClose", true);
         }
+
     }
 
     private void FacePlayer()

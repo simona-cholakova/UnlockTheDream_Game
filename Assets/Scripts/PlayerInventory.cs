4c9326b79bebe19394ad4053a7434c6af6b0285f
simona-cholakova <colakovas@yahoo.com>
2026-02-15 16:12:46 +0100

    Shield appears in front of player, when pressed "F", if it is collected

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -4,10 +4,21 @@ public class PlayerInventory : MonoBehaviour
 {
     public static PlayerInventory instance;
     public bool hasShield = false;
+    public GameObject equippedShield;
+    private bool shieldActive = false;
 
     void Awake()
     {
         instance = this;
+    }
+
+    void Update()
+    {
+        if(hasShield && Input.GetKeyDown(KeyCode.F))
+        {
+            shieldActive = !shieldActive;
+            equippedShield.SetActive(shieldActive);
+        }
     }
     
 }
